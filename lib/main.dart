79ab227d3b8e1868a3a0a6933646bb81e61f2f50
Abdirauf Mohamed <dalkam505@gmail.com>
2022-05-25 09:28:20 +0300

    Second Commit

--- lib/main.dart
+++ lib/main.dart
@@ -13,12 +13,14 @@ class ProfileView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        alignment: Alignment.center,
-        children: [
-          Column(
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: SizedBox(
+          height: 900,
+          child: Stack(
+            alignment: Alignment.center,
             children: [
-              Stack(
+              Column(
                 children: [
                   Container(
                     width: double.infinity,
@@ -46,81 +48,309 @@ class ProfileView extends StatelessWidget {
                         ),
                       ),
                     ),
-                  ),
+                  )
                 ],
-              )
-            ],
-          ),
-          Positioned(
-            top: 160,
-            child: Container(
-              height: MediaQuery.of(context).size.height * 0.9,
-              width: MediaQuery.of(context).size.width,
-              decoration: const BoxDecoration(
-                color: Color(0xffFAFAFA),
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(12),
-                  topRight: Radius.circular(12),
-                ),
               ),
-            ),
-          ),
-          Positioned(
-            top: 130,
-            child: Column(
-              children: [
-                Container(
-                  width: 100,
-                  height: 100,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(50),
-                    color: Colors.red,
-                  ),
-                ),
-                const Text('Sign in'),
-                const SizedBox(
-                  height: 30,
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(
-                    left: 10,
-                  ),
-                  child: SizedBox(
-                    width: MediaQuery.of(context).size.width,
-                    child: Column(
-                      children: [
-                        Container(
-                          margin: const EdgeInsets.symmetric(horizontal: 10),
-                          decoration: BoxDecoration(
-                              color: Theme.of(context).cardColor,
-                              borderRadius: BorderRadius.circular(12),
-                              boxShadow: [
-                                BoxShadow(
-                                  color: Theme.of(context).cardColor,
-                                  offset: const Offset(0, 0),
-                                  spreadRadius: 0,
-                                  blurRadius: 7,
-                                ),
-                                BoxShadow(
-                                  color: Colors.grey.shade200,
-                                  offset: const Offset(0, 0),
-                                  spreadRadius: 0,
-                                  blurRadius: 7,
-                                ),
-                              ]),
-                          child: const ListTile(
-                            title: Text('Dark Mode'),
-                            trailing: Icon(Icons.light_mode),
-                          ),
-                        )
-                      ],
+              Positioned(
+                top: 160,
+                child: Container(
+                  height: MediaQuery.of(context).size.height * 0.12,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: const BoxDecoration(
+                    color: Color(0xffFAFAFA),
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(12),
+                      topRight: Radius.circular(12),
                     ),
                   ),
-                )
-              ],
-            ),
+                ),
+              ),
+              Positioned(
+                top: 130,
+                child: Column(
+                  children: [
+                    Container(
+                      width: 100,
+                      height: 100,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        color: Colors.red,
+                      ),
+                    ),
+                    const Text('Sign in'),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        left: 10,
+                      ),
+                      child: SizedBox(
+                        width: MediaQuery.of(context).size.width,
+                        child: SingleChildScrollView(
+                          child: Column(
+                            children: [
+                              Container(
+                                margin:
+                                    const EdgeInsets.symmetric(horizontal: 10),
+                                decoration: BoxDecoration(
+                                    color: Theme.of(context).cardColor,
+                                    borderRadius: BorderRadius.circular(12),
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: Theme.of(context).cardColor,
+                                        offset: const Offset(0, 0),
+                                        spreadRadius: 0,
+                                        blurRadius: 7,
+                                      ),
+                                      BoxShadow(
+                                        color: Colors.grey.shade200,
+                                        offset: const Offset(0, 0),
+                                        spreadRadius: 0,
+                                        blurRadius: 7,
+                                      ),
+                                    ]),
+                                child: const ListTile(
+                                  title: Text('Dark Mode'),
+                                  trailing: Icon(Icons.light_mode),
+                                ),
+                              ),
+                              Container(
+                                margin:
+                                    const EdgeInsets.symmetric(horizontal: 10),
+                                decoration: BoxDecoration(
+                                    color: Theme.of(context).cardColor,
+                                    borderRadius: BorderRadius.circular(12),
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: Theme.of(context).cardColor,
+                                        offset: const Offset(0, 0),
+                                        spreadRadius: 0,
+                                        blurRadius: 7,
+                                      ),
+                                      BoxShadow(
+                                        color: Colors.grey.shade200,
+                                        offset: const Offset(0, 0),
+                                        spreadRadius: 0,
+                                        blurRadius: 7,
+                                      ),
+                                    ]),
+                                child: const ListTile(
+                                  title: Text('Dark Mode'),
+                                  trailing: Icon(Icons.light_mode),
+                                ),
+                              ),
+                              Container(
+                                margin:
+                                    const EdgeInsets.symmetric(horizontal: 10),
+                                decoration: BoxDecoration(
+                                    color: Theme.of(context).cardColor,
+                                    borderRadius: BorderRadius.circular(12),
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: Theme.of(context).cardColor,
+                                        offset: const Offset(0, 0),
+                                        spreadRadius: 0,
+                                        blurRadius: 7,
+                                      ),
+                                      BoxShadow(
+                                        color: Colors.grey.shade200,
+                                        offset: const Offset(0, 0),
+                                        spreadRadius: 0,
+                                        blurRadius: 7,
+                                      ),
+                                    ]),
+                                child: const ListTile(
+                                  title: Text('Dark Mode'),
+                                  trailing: Icon(Icons.light_mode),
+                                ),
+                              ),
+                              Container(
+                                margin:
+                                    const EdgeInsets.symmetric(horizontal: 10),
+                                decoration: BoxDecoration(
+                                    color: Theme.of(context).cardColor,
+                                    borderRadius: BorderRadius.circular(12),
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: Theme.of(context).cardColor,
+                                        offset: const Offset(0, 0),
+                                        spreadRadius: 0,
+                                        blurRadius: 7,
+                                      ),
+                                      BoxShadow(
+                                        color: Colors.grey.shade200,
+                                        offset: const Offset(0, 0),
+                                        spreadRadius: 0,
+                                        blurRadius: 7,
+                                      ),
+                                    ]),
+                                child: const ListTile(
+                                  title: Text('Dark Mode'),
+                                  trailing: Icon(Icons.light_mode),
+                                ),
+                              ),
+                              Container(
+                                margin:
+                                    const EdgeInsets.symmetric(horizontal: 10),
+                                decoration: BoxDecoration(
+                                    color: Theme.of(context).cardColor,
+                                    borderRadius: BorderRadius.circular(12),
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: Theme.of(context).cardColor,
+                                        offset: const Offset(0, 0),
+                                        spreadRadius: 0,
+                                        blurRadius: 7,
+                                      ),
+                                      BoxShadow(
+                                        color: Colors.grey.shade200,
+                                        offset: const Offset(0, 0),
+                                        spreadRadius: 0,
+                                        blurRadius: 7,
+                                      ),
+                                    ]),
+                                child: const ListTile(
+                                  title: Text('Dark Mode'),
+                                  trailing: Icon(Icons.light_mode),
+                                ),
+                              ),
+                              Container(
+                                margin:
+                                    const EdgeInsets.symmetric(horizontal: 10),
+                                decoration: BoxDecoration(
+                                    color: Theme.of(context).cardColor,
+                                    borderRadius: BorderRadius.circular(12),
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: Theme.of(context).cardColor,
+                                        offset: const Offset(0, 0),
+                                        spreadRadius: 0,
+                                        blurRadius: 7,
+                                      ),
+                                      BoxShadow(
+                                        color: Colors.grey.shade200,
+                                        offset: const Offset(0, 0),
+                                        spreadRadius: 0,
+                                        blurRadius: 7,
+                                      ),
+                                    ]),
+                                child: const ListTile(
+                                  title: Text('Dark Mode'),
+                                  trailing: Icon(Icons.light_mode),
+                                ),
+                              ),
+                              Container(
+                                margin:
+                                    const EdgeInsets.symmetric(horizontal: 10),
+                                decoration: BoxDecoration(
+                                    color: Theme.of(context).cardColor,
+                                    borderRadius: BorderRadius.circular(12),
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: Theme.of(context).cardColor,
+                                        offset: const Offset(0, 0),
+                                        spreadRadius: 0,
+                                        blurRadius: 7,
+                                      ),
+                                      BoxShadow(
+                                        color: Colors.grey.shade200,
+                                        offset: const Offset(0, 0),
+                                        spreadRadius: 0,
+                                        blurRadius: 7,
+                                      ),
+                                    ]),
+                                child: const ListTile(
+                                  title: Text('Dark Mode'),
+                                  trailing: Icon(Icons.light_mode),
+                                ),
+                              ),
+                              Container(
+                                margin:
+                                    const EdgeInsets.symmetric(horizontal: 10),
+                                decoration: BoxDecoration(
+                                    color: Theme.of(context).cardColor,
+                                    borderRadius: BorderRadius.circular(12),
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: Theme.of(context).cardColor,
+                                        offset: const Offset(0, 0),
+                                        spreadRadius: 0,
+                                        blurRadius: 7,
+                                      ),
+                                      BoxShadow(
+                                        color: Colors.grey.shade200,
+                                        offset: const Offset(0, 0),
+                                        spreadRadius: 0,
+                                        blurRadius: 7,
+                                      ),
+                                    ]),
+                                child: const ListTile(
+                                  title: Text('Dark'),
+                                  trailing: Icon(Icons.light_mode),
+                                ),
+                              ),
+                              Container(
+                                margin:
+                                    const EdgeInsets.symmetric(horizontal: 10),
+                                decoration: BoxDecoration(
+                                    color: Theme.of(context).cardColor,
+                                    borderRadius: BorderRadius.circular(12),
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: Theme.of(context).cardColor,
+                                        offset: const Offset(0, 0),
+                                        spreadRadius: 0,
+                                        blurRadius: 7,
+                                      ),
+                                      BoxShadow(
+                                        color: Colors.grey.shade200,
+                                        offset: const Offset(0, 0),
+                                        spreadRadius: 0,
+                                        blurRadius: 7,
+                                      ),
+                                    ]),
+                                child: const ListTile(
+                                  title: Text('Dark Mode'),
+                                  trailing: Icon(Icons.light_mode),
+                                ),
+                              ),
+                              Container(
+                                margin:
+                                    const EdgeInsets.symmetric(horizontal: 10),
+                                decoration: BoxDecoration(
+                                    color: Theme.of(context).cardColor,
+                                    borderRadius: BorderRadius.circular(12),
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: Theme.of(context).cardColor,
+                                        offset: const Offset(0, 0),
+                                        spreadRadius: 0,
+                                        blurRadius: 7,
+                                      ),
+                                      BoxShadow(
+                                        color: Colors.grey.shade200,
+                                        offset: const Offset(0, 0),
+                                        spreadRadius: 0,
+                                        blurRadius: 7,
+                                      ),
+                                    ]),
+                                child: const ListTile(
+                                  title: Text('Dark m'),
+                                  trailing: Icon(Icons.light_mode),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
